7a57f624987ff1fecf06b9c84a63f9e99b47816b
Alexander Roshchin <alexander.roshchin85@gmail.com>
2020-08-05 21:59:35 +0300

    Add Month component

--- src/components/Calendar/index.js
+++ src/components/Calendar/index.js
@@ -1,5 +1,7 @@
-import React, {Component} from 'react';
+import React, {Component} from 'react'
 import styles from './Calendar.module.scss'
+import * as dateFns from 'date-fns'
+import Month from './../Month'
 
 class Calendar extends Component {
     constructor(props) {
@@ -12,10 +14,57 @@ class Calendar extends Component {
 
     render() {
         const {currentDate} = this.state
+        const startMonth = dateFns.startOfMonth(currentDate)
+        const endMonth = dateFns.endOfMonth(currentDate)
+        const fullMonth = dateFns.eachDayOfInterval({
+            start: startMonth,
+            end: endMonth
+        })
+        const fullMonthDates = fullMonth.map((day) => {
+            return dateFns.format(day, 'd')
+        })
         return (
-            <div>
-                {currentDate.toLocaleDateString()}
-            </div>
+            <article>
+                <div>
+                    <Month month={fullMonthDates}/>
+                    {currentDate.toLocaleDateString()}
+                </div>
+                <button onClick={() => {
+                    this.setState({
+                        currentDate: dateFns.add(currentDate, {
+                            years: 1
+                        })
+                    })
+                }}
+                >
+                    add year
+                </button>
+
+                <button onClick={() => {
+                    this.setState({
+                        currentDate: dateFns.add(currentDate, {
+                            months: 1
+                        })
+                    })
+                }}>add month</button>
+
+                <button onClick={() => {
+                    this.setState({
+                        currentDate: dateFns.add(currentDate, {
+                            weeks: 1
+                        })
+                    })
+                }}>add week</button>
+
+                <button onClick={() => {
+                    this.setState({
+                        currentDate: dateFns.add(currentDate, {
+                            days: 1
+                        })
+                    })
+                }}>add day</button>
+            </article>
+
         );
     }
 }
---
+++ src/components/Month/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Month(props) {
+    const {year, month} = props
+
+    return (
+        <div>
+            {month}
+        </div>
+    )
+}
+
+export default Month
