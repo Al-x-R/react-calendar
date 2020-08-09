c7cef367ec074b865ffe08c4692da6a09afc85fb
Alexander Roshchin <alexander.roshchin85@gmail.com>
2020-08-09 18:46:11 +0300

    Add propTypes to component Week

--- src/components/Calendar/Week/index.js
+++ src/components/Calendar/Week/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
-import {addDays, format,
-} from 'date-fns'
+import {addDays, format, startOfWeek} from 'date-fns'
 import Day from '../Day'
+import PropTypes from "prop-types";
 
 
 function Week(props) {
-    const {weekStart} = props
+    const {date, currentDate} = props
 
+    const weekStart = startOfWeek(date)
     const weekDays = []
     for (let i = 1; i <= 7; i++) {
         weekDays.push(addDays(weekStart, i))
@@ -20,6 +21,16 @@ function Week(props) {
         </tr>
 
     )
+}
+
+Week.propTypes = {
+    date: PropTypes.instanceOf(Date),
+    currentDate: PropTypes.instanceOf(Date),
+}
+
+Week.defaultProps = {
+    date: new Date(),
+    currentDate: new Date(),
 }
 
 export default Week
