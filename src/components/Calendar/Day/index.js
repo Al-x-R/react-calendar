a7ea7d75b98c02ed194d794d4eaea671f9e47d67
Alexander Roshchin <alexander.roshchin85@gmail.com>
2020-08-10 12:25:26 +0300

    Add propTypes to component Day

--- src/components/Calendar/Day/index.js
+++ src/components/Calendar/Day/index.js
@@ -1,10 +1,30 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import classNames from 'classnames'
+import {isSameDay, isSameMonth} from "date-fns"
 import styles from './Day.module.scss'
 
 function Day(props) {
-const {day} = props
+    const {day, date, currentDate, className} = props
 
-    return (<td className={styles.day}>{day}</td>)
+    const classNameStr = classNames(className, {
+        [styles.currentDate]: isSameDay(date, currentDate),
+        [styles.currentMonth]: isSameMonth(date, currentDate),
+    })
+    console.log(classNameStr)
+
+    return (
+        <td className={classNameStr}>{day}</td>
+    )
+}
+
+Day.propTypes = {
+    day: PropTypes.string.isRequired,
+    className: PropTypes.string,
+}
+
+Day.defaultProps = {
+    className: styles.day,
 }
 
 export default Day
